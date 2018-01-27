72de5479961c73a374383e09d78aea4030f64e30
DESKTOP-038JESQ\Caleb Ljunggren <CLjunggren@my.gcu.edu>
2018-01-26 18:30:29 -0700

    Changes made that add code done from In-Class Activity for a basic login page with a login failed/passed response functionality.

---
+++ Minesweeper/Minesweeper/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Minesweeper.Models;
+using Minesweeper.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Minesweeper.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            SecurityService service = new SecurityService();
+            if (service.Authenticate(model))
+                return View("LoginPassed");
+            else
+                return View("LoginFailed", model);
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper/Services/Data/SecurityDAO.cs
@@ -0,0 +1,58 @@
+﻿using Minesweeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Minesweeper.Services.Data
+{
+    public class SecurityDAO
+    {
+        //Setup connection string
+        string connectionString =
+            "Data Source=(localdb)\\MSSQLLocalDB;initial catalog=Test;Integrated Security=SSPI;";
+
+
+        public bool FindByUser(UserModel user)
+        {
+            bool result = false;
+
+            try
+            {
+                //Setup SELECT query with parameters
+                string query = "SELECT * FROM dbo.Users WHERE USERNAME=@Username AND PASSWORD=@Password";
+
+                //Create connection and command
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand(query, cn))
+
+                {
+                    //Set query parameters and their values
+                    cmd.Parameters.Add("@Username", SqlDbType.VarChar, 50).Value = user.Username;
+                    cmd.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = user.Password;
+
+                    //Open the connection
+                    cn.Open();
+
+                    //Using a DataReader, see if query returns any rows
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                        result = true;
+                    else
+                        result = false;
+
+                    //Close the connection
+                    cn.Close();
+                }
+                return result;
+            }
+            catch (SqlException e)
+            {
+                // TODO: should log exception and then throw a custom exception
+                throw e;
+            }
+        }
+    }
+}
