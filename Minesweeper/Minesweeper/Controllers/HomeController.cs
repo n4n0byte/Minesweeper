fa82018adfe1fc4a3a516f4382999d58537a7f97
Ali Cooper <coopnukem@gmail.com>
2018-01-28 22:08:23 -0700

    changed UI and docs

--- Minesweeper/Minesweeper/Controllers/HomeController.cs
+++ Minesweeper/Minesweeper/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Minesweeper.Controllers
         {
             return RedirectToAction("Index","Login");
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
