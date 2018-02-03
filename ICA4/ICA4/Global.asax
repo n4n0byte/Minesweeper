d0da1abb306592c4c6470e1264fa951a4eed2f21
Ali Cooper <coopnukem@gmail.com>
2018-02-03 15:28:23 -0700

    update

---
+++ ICA4/ICA4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ICA4.MvcApplication" Language="C#" %>
---
+++ ICA4/ICA4/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ICA4.Models {
+    public class UserModel {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public UserModel(string name, string emailAddress, string phoneNumber) {
+            Name = name;
+            EmailAddress = emailAddress;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
