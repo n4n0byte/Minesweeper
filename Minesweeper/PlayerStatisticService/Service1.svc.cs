816ff2f533e01d733a1f237e820096d677667deb
Ali Cooper <coopnukem@gmail.com>
2018-04-10 18:13:38 -0700

    updated to to have foreign key, and seperated the player variables into individual columns

--- Minesweeper/PlayerStatisticService/Service1.svc.cs
+++ Minesweeper/PlayerStatisticService/Service1.svc.cs
@@ -20,6 +20,8 @@ namespace PlayerStatisticService
         }
 
         public DTO GetAllPlayerStats() {
+            // create default constructed DTO
+            // 
             throw new NotImplementedException();
         }
 
