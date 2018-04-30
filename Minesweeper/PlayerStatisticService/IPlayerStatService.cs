5961f469de5743e24a8d403d2bb21a880e33126d
Ali Cooper <coopnukem@gmail.com>
2018-04-29 18:05:05 -0700

    added more documentation

--- Minesweeper/PlayerStatisticService/IPlayerStatService.cs
+++ Minesweeper/PlayerStatisticService/IPlayerStatService.cs
@@ -8,15 +8,28 @@ using System.Text;
 
 namespace PlayerStatisticService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPlayerStatService" in both code and config file together.
+    /// <summary>
+    /// Interface For Player Stat Rest service
+    /// </summary>
     [ServiceContract]
     public interface IPlayerStatService
     {
 
+        /// <summary>
+        /// Returns All PlayerStats in a List 
+        /// which is then stored in a DTO
+        /// </summary>
+        /// <returns>DTO</returns>
         [OperationContract]
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetAllPlayerStats/")]
         DTO GetAllPlayerStats();
 
+        /// <summary>
+        /// Returns A DTO Containing containing a 
+        /// game, found by User ID
+        /// </summary>
+        /// <param name="id">User ID</param>
+        /// <returns>DTO</returns>
         [OperationContract]
         [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetPlayerStat/{id}")]
         DTO GetPlayerStat(string id);
